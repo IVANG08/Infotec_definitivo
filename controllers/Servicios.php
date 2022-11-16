f483dd1ec57bb53210aa078c50b4f3d3f78dde8e
9anavaleria <ana98cip@gmail.com>
2022-11-15 19:51:20 -0500

    Creación de controladores jhoan

--- controllers/Servicios.php
+++ controllers/Servicios.php
@@ -1,24 +1,24 @@
 <?php
-class Persona{
+    class Servicios{
         public function __construct(){}
         public function index(){
             require_once "views/roles/admin/header_dash.php";
-            require_once "views/modules/1_users/user_principal_view.php";
+            require_once "views/modules/2_products/2_3servicio/servicios_principal_view.php";
             require_once "views/roles/admin/footer.php";
         }
         public function crear(){
             require_once "views/roles/admin/header_dash.php";
-            require_once "views/modules/1_users/user_new.php";
+            require_once "views/modules/2_products/2_3servicio/crear_servicios.php";
             require_once "views/roles/admin/footer.php";
         }
         public function consultar(){
             require_once "views/roles/admin/header_dash.php";
-            require_once "views/modules/1_users/user_view.php";
+            require_once "views/modules/2_products/2_3servicio/consultar_servicio.php";
             require_once "views/roles/admin/footer.php";
         }
         public function actualizar(){
             require_once "views/roles/admin/header_dash.php";
-            require_once "views/modules/1_users/user_actualizar.php";
+            require_once "views/modules/2_products/2_3servicio/actualizar_servicio.php";
             require_once "views/roles/admin/footer.php";
         }
     }
