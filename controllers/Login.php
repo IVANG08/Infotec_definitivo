5deb39ddcda72bcf01ba82ac39bc3818df975acc
9anavaleria <ana98cip@gmail.com>
2023-08-22 18:45:31 -0500

    Actualización base de datos

--- controllers/Login.php
+++ controllers/Login.php
@@ -1,41 +1,26 @@
 <?php
-session_start();
-
 require_once "models/dto_model/Login_dto.php";
+require_once "models/dao_model/Login_dao.php";
 class Login{
     private $loginDao;
-        public function __construct(){
-            $this->loginDao = new Login_dao;
-        }
-}
-
-$dsn = "mysql:host=localhost;dbname=nombre_base_de_datos";
-$username = "usuario";
-$password = "contraseña";
-
-try {
-    $db = new PDO($dsn, $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $model = new Model($db);
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        $user = $model->getUserByUsername($username);
-
-        if ($user && password_verify($password, $user["password"])) {
-            $_SESSION["user_id"] = $user["id"];
-            header("Location: dashboard.php");
-            exit;
-        } else {
-            $error = "Invalid username or password";
-        }
+    public function __construct(){
+        $this->loginDao = new Login_dao;
     }
+    public function index(){
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                
+            require_once "views/business/inicio.php";
+        }
+        elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Capturar Datos
+            // Validar los Datos
+            // Crear el Objeto
+            // Comprobar en la base de datos
+            // Redireccionar al Dashboard
+            header("Location: ?c=Dashboard");
+        }
 
-    include "view.php";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
+    }
+    
 }
 ?>
