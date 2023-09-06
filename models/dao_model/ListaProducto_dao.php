5bd6f697bb4284bcb244690562e9dc81717310ef
9anavaleria <95309779+9anavaleria@users.noreply.github.com>
2023-09-06 14:11:27 -0500

    Update ListaProducto_dao.php

---
+++ models/dao_model/ListaProducto_dao.php
@@ -0,0 +1,27 @@
+<?php
+    class ListaProducto_dao{
+        private $pdo;
+        
+		public function __construct(){
+			try {
+				$db = new DataBase();
+				$this->pdo = $db->connection();				
+
+			} catch (Exception $e) {
+				die($e->getMessage());
+			}
+		}	
+        public function crearListaProduto($listaProducto_dto){
+            try {
+                $sql = "INSERT INTO `lista_productos_f`(`id_listP`, `id_factura`, `id_producto`, `cantidad`, `valor_venta`) VALUES (?,?,?,?,?)";
+                $resultado = $this->pdo->prepare($sql);
+                $resultado->execute(array('',$listaProducto_dto->getIdFac(),$listaProducto_dto->getIdProducto(),$listaProducto_dto->getCantProducto(),$listaProducto_dto->getValorProducto()));
+                print_r($resultado);
+                exit;
+                return $resultado->rowCount();
+            }catch (Exception $e) {
+                die("....".$e->getMessage());	
+            }
+        }
+    }
+?>
